5fa5300336ce743c3691a5fd0d0fab44d7c595cf
Lázaro Pimentel <118478127+LazaroAmvox@users.noreply.github.com>
2024-01-14 22:11:07 -0300

    first commit

---
+++ src/core/components/footer/index.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import About from '@/core/components/modal-provably-fair'
+import { ChatBubbleLeftRightIcon } from '@heroicons/react/24/outline'
+
+export default function Footer({}) {
+  const [showModal, setShowModal] = useState<boolean>(false)
+
+  return (
+    /* sm:w-[33%] xl:w-[25%] w-[100%] */
+    <div
+      className={`fixed z-[999] bottom-0 left-0 flex  bg-black h-10 border-[1px] border-gray-700 relative w-full`}
+    >
+      <div className='flex items-center gap-2 bg-orange-600 my-2 mx-2 px-2 rounded-md text-white cursor-pointer'>
+        <ChatBubbleLeftRightIcon className='h-5 w-5'/>
+          <p className='text-sm'>Chat</p>
+      </div>
+
+      <div className="flex items-center flex-grow">
+        <p className="pl-2 text-white  text-[12px] md:text-[7px] lg:text-[9px] font-thin">
+          This game is{' '}
+        </p>
+        <i className="fi fi-rs-shield-check pl-1 text-green-500 items-center">
+          <img
+            src="../images/icons/SecureLogo.svg"
+            className="h-4 mx-auto"
+          />
+        </i>
+        <button
+          onClick={() => {
+            setShowModal(!showModal)
+          }}
+          className="pl-1 text-[12px] md:text-[7px] lg:text-[9px] text-white items-center"
+        >
+          <span className="">Provably Fair</span>
+        </button>
+      </div>
+      <div className="flex items-center pr-3">
+        <p className="text-white text-[12px] md:text-[7px] lg:text-[9px] font-thin">
+          powered by
+        </p>
+        <p className="pl-1  text-[12px] md:text-[7px] lg:text-[9px] text-white underline">
+          <a
+            href="https://hypetech.games/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            HYPETECH
+          </a>
+        </p>
+      </div>{' '}
+      <About show={showModal} toggle={setShowModal} />
+    </div>
+  )
+}
